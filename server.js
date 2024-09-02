dff00a0384f9befddc22b4493b448805951315ed
Alpha <111243437+kbrmani@users.noreply.github.com>
2024-09-02 19:22:43 +0530

    update frontend path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { giveCollection } = require('./client')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '../../')));
+app.use(express.static(path.join(__dirname, '../../zimeshare_assignment')));
 
 app.get('/api/products', async (req, res) => {
 
