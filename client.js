ce1a9f3e826c837d9f19738385bba7fc330ba3d0
Alpha <111243437+kbrmani@users.noreply.github.com>
2024-09-02 18:50:47 +0530

    initial commit

---
+++ client.js
@@ -0,0 +1,29 @@
+// In `config/client.js`
+const { MongoClient } = require("mongodb");
+
+async function client() {
+  try{
+    const client = new MongoClient("mongodb+srv://kbrmanish01:VrI6r7rR04Xud1k9@cluster0.pxxma.mongodb.net/");
+    await client.connect();
+    return client;
+  }
+  catch(error){
+    console.log(error);
+  }
+ 
+}
+
+module.exports = client;
+
+const giveCollection = async (collectionName) => {
+  try {
+    const clientConnect = await client();
+    const db = clientConnect.db("Zimeshare_Assignment");
+    const collection = db.collection(collectionName);
+    return collection;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports = { giveCollection };
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const path = require('path');
+const { giveCollection } = require('./client')
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '../../')));
+
+app.get('/api/products', async (req, res) => {
+
+  try {
+
+    const products = await giveCollection("Cloths_products")
+    console.log(products);
+    const data = await products.find().toArray()
+    console.log(data);
+    return res.json(data);
+
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
